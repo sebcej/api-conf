714b549f4ebb658f878831837a1b0dbd397dac74
sebcej <sebacej.92@gmail.com>
2022-10-24 18:38:15 +0200

    feat: new tests, better edge cases handling, error management

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,6 +1,6 @@
 import { parseIfAvailable } from "./utils"
 
-export async function fetchData(fetcher, fetcherConfig = {}, routeConfig = {}, initialData = {}) {
+export async function fetchData(fetcher, fetcherConfig = {}, routeConfig = {}, initialData) {
     const data = parseIfAvailable(routeConfig.parseRequestData, initialData, initialData, routeConfig, fetcherConfig)
 
     // Create main configuration with default values.
@@ -8,13 +8,26 @@ export async function fetchData(fetcher, fetcherConfig = {}, routeConfig = {}, i
     const requestConfig = {
         method: routeConfig.method || 'GET',
         url: parseIfAvailable(routeConfig.url, null, data, routeConfig, fetcherConfig),
+        data,
         ...routeConfig.config
     }
 
     let conf = parseIfAvailable(routeConfig.parseRequestConfig, requestConfig, requestConfig, routeConfig, data, fetcherConfig)
     conf = parseIfAvailable(fetcherConfig.beforeRequest, conf, conf)
 
-    let response = await fetcher(conf)
+    let response
+
+    try {
+        response = await fetcher(conf)
+    } catch (e) {
+        if (routeConfig.onError) {
+            response = parseIfAvailable(routeConfig.onError, null, e, conf, fetcherConfig)
+        } else if (fetcherConfig.onError) {
+            response = parseIfAvailable(fetcherConfig.onError, null, e, conf, fetcherConfig)
+        } else {
+            throw e
+        }
+    }
 
     response = parseIfAvailable(fetcherConfig.afterResponse, response, response, conf)
     response = parseIfAvailable(routeConfig.parseResponseData, response, response, conf)
--- test/fetcher.test.js
+++ test/fetcher.test.js
@@ -15,7 +15,7 @@ describe('fetchData', function () {
         expect(response).to.have.property('test')
 
         expect(fetcher.called)
-        expect(fetcher.calledWithMatch(config))
+        expect(fetcher.getCall(0).args[0]).to.nested.include(config)
     })
 
     it('Should parse returned value', async function () {
@@ -33,10 +33,10 @@ describe('fetchData', function () {
         expect(response).to.have.property('nested')
 
         expect(fetcher.called)
-        expect(fetcher.calledWithMatch(config))
+        expect(fetcher.getCall(0).args[0]).to.include(config)
     })
 
-    it('Should perform fetch normally', async function () {
+    it('Should replace beforeRequest', async function () {
         const fetcher = sinon.fake.returns({ test: true });
         const config = {
             method: 'GET',
@@ -65,6 +65,95 @@ describe('fetchData', function () {
         expect(response).to.have.property('test')
 
         expect(fetcher.called)
-        expect(fetcher.calledWithMatch({...config, url: 'newurl'}))
+        expect(fetcher.getCall(0).args[0]).to.nested.include({url: 'test'})
+    })
+
+    it('Should replace url before request', async function () {
+        const fetcher = sinon.fake.returns({ test: true });
+        const config = {
+            method: 'GET',
+            url: 'test',
+            parseRequestConfig: conf => ({...conf, url: 'replaced'})
+        }
+
+        const response = await fetchData(fetcher, undefined, config, undefined)
+
+        expect(response).to.have.property('test')
+
+        expect(fetcher.called)
+        expect(fetcher.getCall(0).args[0]).to.nested.include({url: 'replaced'})
+    })
+
+    it('Should replace data before request', async function () {
+        const fetcher = sinon.fake.returns({ test: true });
+        const config = {
+            method: 'GET',
+            url: 'test',
+            parseRequestData: data => ({...data, extraData: true})
+        }
+
+        const response = await fetchData(fetcher, undefined, config, {normalData: true})
+
+        expect(response).to.have.property('test')
+
+        expect(fetcher.called)
+        expect(fetcher.getCall(0).args[0]).to.have.nested.property('data.extraData')
+    })
+
+    it('Should replace data after request', async function () {
+        const fetcher = sinon.fake.returns({ test: true });
+        const config = {
+            method: 'GET',
+            url: 'test',
+            parseResponseData: data => ({...data, extraData: true})
+        }
+
+        const response = await fetchData(fetcher, undefined, config, undefined)
+
+        expect(response).to.have.property('test')
+        expect(response).to.have.property('extraData')
+
+        expect(fetcher.called)
+    })
+
+    it('Should throw error on failure', async function () {
+        const fetcher = sinon.fake.throws();
+        const config = {
+            method: 'GET',
+            url: 'test'
+        }
+
+        let throwed = false
+
+        try{
+            await fetchData(fetcher, undefined, config, undefined)
+        } catch (e) {
+            throwed = true
+        }
+
+        expect(throwed).to.be.true
+    })
+
+    it('Should not throw error on failure', async function () {
+        const fetcher = sinon.fake.throws();
+        const config = {
+            method: 'GET',
+            url: 'test'
+        }
+        const fetcherConfig = {
+            onError: () => ({ success: false })
+        }
+
+        let throwed = false
+        let response
+
+        try{
+            response = await fetchData(fetcher, fetcherConfig, config, undefined)
+        } catch (e) {
+            throwed = true
+        }
+
+        expect(throwed).to.be.false
+        expect(response).to.have.property('success')
     })
 })
